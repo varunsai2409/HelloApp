06c70974ee3ca4c761d84d49d5afec2a11e495b2
varunsai2409 <varunsaithokala2008@gmail.com>
2026-03-26 15:36:59 +0530

    UC2: Display Hello with Command-Line Argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,19 @@
 /**
- * UC1: Display "Hello World"
- * * @author Varun sai
+ * UC2: Display "Hello" with Command-Line Argument
+ *  @author Varun sai
  * @version 2
  */
-public class HelloApp {
+public class HelloApp
+{
     
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        if (args.length > 0)
+        {
+            System.out.println("Hello " + args[0]);
+        } else
+        {
+            System.out.println("Hello");
+        }
     }
-}
+}	
