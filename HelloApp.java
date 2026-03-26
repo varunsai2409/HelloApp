9cf3e6427235d71582980681899091f4b6e4ec5d
varunsai2409 <varunsaithokala2008@gmail.com>
2026-03-26 13:48:49 +0530

    UC1: Display Hello world

---
+++ HelloApp.java
@@ -0,0 +1,11 @@
+/**
+ * UC1: Display "Hello World"
+ * * @author Varun sai
+ * @version 2
+ */
+public class HelloApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
